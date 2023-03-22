3d7c805df3eb824048ad6ff0cdef31ee2172aa9d
HugoPStowasser <hugopstowasser@gmail.com>
2023-03-22 15:40:39 -0300

    Criação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Login } from './pages/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  )
+}
+
+export default App
