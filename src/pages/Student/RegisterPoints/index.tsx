0110e46069ce80985d61e4032bb4abdc4917b73f
Hugo Stowasser <hugopstowasser@gmail.com>
2023-06-24 22:43:35 -0300

    UI pronta, faltando lógica e integração

--- src/pages/Student/RegisterPoints/index.tsx
+++ src/pages/Student/RegisterPoints/index.tsx
@@ -1,5 +1,2 @@
-import { Text } from "@chakra-ui/react";
-
-export const RegisterPoints = () => {
-  return <Text>Cadastrar pontos</Text>;
-};
+import { RegisterPoints } from "./RegisterPoints";
+export { RegisterPoints };
