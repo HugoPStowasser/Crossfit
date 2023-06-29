1505aaff8391458d25a915f409fcc2c0f582b97a
Hugo Stowasser <hugopstowasser@gmail.com>
2023-06-28 23:22:25 -0300

    Return refactor

--- src/pages/Student/StudentPoints/StudentPoints.tsx
+++ src/pages/Student/StudentPoints/StudentPoints.tsx
@@ -81,100 +81,98 @@ export const StudentPoints = () => {
   return (
     <Box p="15px">
     <TitleWithBackButton title={`Adicionar pontuação`} />
-  
-    <Box
-      display={"flex"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      flexDir={"column"}
-    >
-      <Box width={"80%"} maxW={"720px"}>
-        <Box mt="100px">
-          <FormControl mb={4} pt={20}>
-            <FormLabel>Exercício</FormLabel>
-              <Select value={selectedExercise || ''} onChange={handleExerciseChange}>
-                {exercise.map((exercise) => (
-                  <option key={exercise.idExercise} value={exercise.idExercise}>
-                    {exercise.description}
-                  </option>
-                ))}
-              </Select>
-          </FormControl>
 
-          <FormControl mb={4}>
-            <FormLabel>Pontuação</FormLabel>
-            <Flex align="center">
-              <IconButton
-                icon={<MinusIcon />}
-                onClick={handleDecrement}
-                aria-label="Decrement"
-                mr={2}
-              />
-              <NumberInput
-                value={score}
-                onChange={handleScoreChange}
-                min={0}
-                w="100%"
-                allowMouseWheel
-              >
-                <NumberInputField />
-                <NumberInputStepper></NumberInputStepper>
-              </NumberInput>
-              <IconButton
-                icon={<AddIcon />}
-                onClick={handleIncrement}
-                aria-label="Increment"
-                ml={2}
-              />
-            </Flex>
-          </FormControl>
-  
-          <FormControl mb={4}>
-            <Stack direction="row" spacing={4} justify="space-between">
-              <Button
-                variant="outline"
-                w="100%"
-                onClick={() => handleScoreChange("", score + 5)}
-              >
-                +5
-              </Button>
-              <Button
-                variant="outline"
-                w="100%"
-                onClick={() => handleScoreChange("", score + 10)}
-              >
-                +10
-              </Button>
-              <Button
-                variant="outline"
-                w="100%"
-                onClick={() => handleScoreChange("", score + 20)}
-              >
-                +20
-              </Button>
-            </Stack>
-          </FormControl>
-  
-          <Stack direction="column" spacing={5} pt={5}>
-            <Button
-              colorScheme="white"
-              color="green.500"
-              borderColor="green.500"
-              borderWidth="1px"
-              onClick={handleSaveAndContinue}
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDir="column"
+    >
+      <Box width="80%" maxW="720px" mt="100px">
+        <FormControl mb={4} pt={20}>
+          <FormLabel>Exercício</FormLabel>
+          <Select value={selectedExercise || ''} onChange={handleExerciseChange}>
+            {exercise.map((exercise) => (
+              <option key={exercise.idExercise} value={exercise.idExercise}>
+                {exercise.description}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl mb={4}>
+          <FormLabel>Pontuação</FormLabel>
+          <Flex align="center">
+            <IconButton
+              icon={<MinusIcon />}
+              onClick={handleDecrement}
+              aria-label="Decrement"
+              mr={2}
+            />
+            <NumberInput
+              value={score}
+              onChange={handleScoreChange}
+              min={0}
+              w="100%"
+              allowMouseWheel
             >
-              Salvar e continuar
+              <NumberInputField />
+              <NumberInputStepper></NumberInputStepper>
+            </NumberInput>
+            <IconButton
+              icon={<AddIcon />}
+              onClick={handleIncrement}
+              aria-label="Increment"
+              ml={2}
+            />
+          </Flex>
+        </FormControl>
+
+        <FormControl mb={4}>
+          <Stack direction="row" spacing={4} justify="space-between">
+            <Button
+              variant="outline"
+              w="100%"
+              onClick={() => handleScoreChange("", score + 5)}
+            >
+              +5
             </Button>
             <Button
-              colorScheme="green"
-              color="white"
-              bg="green.500"
-              onClick={handleSaveAndExit}
+              variant="outline"
+              w="100%"
+              onClick={() => handleScoreChange("", score + 10)}
             >
-              Salvar e sair
+              +10
+            </Button>
+            <Button
+              variant="outline"
+              w="100%"
+              onClick={() => handleScoreChange("", score + 20)}
+            >
+              +20
             </Button>
           </Stack>
-        </Box>
+        </FormControl>
+
+        <Stack direction="column" spacing={5} pt={5}>
+          <Button
+            colorScheme="white"
+            color="green.500"
+            borderColor="green.500"
+            borderWidth="1px"
+            onClick={handleSaveAndContinue}
+          >
+            Salvar e continuar
+          </Button>
+          <Button
+            colorScheme="green"
+            color="white"
+            bg="green.500"
+            onClick={handleSaveAndExit}
+          >
+            Salvar e sair
+          </Button>
+        </Stack>
       </Box>
     </Box>
   </Box>
